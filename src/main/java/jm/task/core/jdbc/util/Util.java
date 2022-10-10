3321efcd6bb347885cfad51b0da1aeb9599ff97a
RitmanSA <rsas2001@gmail.com>
2022-10-10 15:34:50 +0300

    PP_1_1_3 done

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,22 +5,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final Connection connection;
+    private static  Connection connection;
 
-static {
-    final String URL = "jdbc:MySQL://localhost:3306/database";
-    final String USER = "root";
-    final String PASSWORD = "root1";
+    private static final String URL = "jdbc:MySQL://localhost:3306/database";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root1";
 
-    try {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection(URL, USER, PASSWORD);
-    } catch (ClassNotFoundException | SQLException e) {
-        throw new RuntimeException("trouble while getting connection");
-    }
-}
 
     public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException("trouble while getting connection");
+        }
         return connection;
     }
 
